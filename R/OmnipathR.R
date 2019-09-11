383ad1703866da07d202c0d48c7c60f85b0d2378
Alberto Valdeolivas <alvaldeolivas@gmail.com>
2019-09-11 19:11:20 +0200

    Bioconductor package
    - Aesthetic modifications to meet  Bioconductor requirements. No content modifications

--- R/OmnipathR.R
+++ R/OmnipathR.R
@@ -46,4 +46,8 @@
 #' 
 #' @docType package
 #' @name OmnipathR
+#' @import methods
 NULL
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
--- R/get_complex_genes.R
+++ R/get_complex_genes.R
@@ -9,8 +9,8 @@
 #'    \code{\link{import_Omnipath_complexes}})
 #' @param select_genes vector containing the genes for whom complexes will be
 #' retrieved (hgnc format).
-#' @param total_match [default=FALSE] logical indicating if the user wants to get all 
-#' the complexes where any of the input genes participate (FALSE) or
+#' @param total_match [default=FALSE] logical indicating if the user wants to 
+#' get all the complexes where any of the input genes participate (FALSE) or
 #' to get only the complexes where all the input genes participate together 
 #' (TRUE)
 #' @export
@@ -20,30 +20,26 @@
 #' query_genes = c("LMNA","BANF1")
 #' complexes_query_genes = get_complex_genes(complexes,query_genes)
 #' @seealso \code{\link{import_Omnipath_complexes}}) 
-get_complex_genes = function(complexes = import_Omnipath_complexes(), 
-                              select_genes = query_genes, 
-                              total_match = FALSE){
-  
-  if(is.null(select_genes)){
-    stop("A vector of genes should be provided")
-  }
-  
-  if (!is.logical(total_match)){
-    stop("total_match parameter should be logical")
-  }
-  
-  
-  if (total_match){
-    complexes_geneset <- 
-      complexes[which(unlist(lapply(strsplit(complexes$components_genesymbols,'-'), 
-                                    function(x){
-                          sum(x %in% query_genes) == length(query_genes)}))),]  
-  } else {
-  complexes_geneset <- 
-    complexes[which(unlist(lapply(strsplit(complexes$components_genesymbols,'-'), 
-                function(x){any(x %in% query_genes)}))),]
-  }
-  
-  return(complexes_geneset)
-  
+get_complex_genes = function(complexes = import_Omnipath_complexes(),
+    select_genes, total_match = FALSE){
+
+    if(is.null(select_genes)){
+        stop("A vector of genes should be provided")
+    }
+
+    if (!is.logical(total_match)){
+        stop("total_match parameter should be logical")
+    }
+
+    if (total_match){
+        complexes_geneset <- 
+        complexes[which(unlist(lapply(strsplit(complexes$components_genesymbols,
+            '-'), function(x){
+            sum(x %in% select_genes) == length(select_genes)}))),]  
+    } else {
+        complexes_geneset <- 
+        complexes[which(unlist(lapply(strsplit(complexes$components_genesymbols,
+            '-'), function(x){any(x %in% select_genes)}))),]
+    }
+    return(complexes_geneset)
 }
