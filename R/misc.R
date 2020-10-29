503221ee78bc2f0b6535bdcad2c674d50b6b9dca
Türei Dénes <turei.denes@gmail.com>
2020-10-29 04:33:50 +0100

    helper method to convert NSE to string

---
+++ R/misc.R
@@ -0,0 +1,36 @@
+#!/usr/bin/env Rscript
+
+#
+#  This file is part of the `OmnipathR` R package
+#
+#  Copyright
+#  2018-2020
+#  Saez Lab, Uniklinik RWTH Aachen, Heidelberg University
+#
+#  File author(s): Alberto Valdeolivas
+#                  Dénes Türei (turei.denes@gmail.com)
+#                  Attila Gábor
+#
+#  Distributed under the MIT (Expat) License.
+#  See accompanying file `LICENSE` or find a copy at
+#      https://directory.fsf.org/wiki/License:Expat
+#
+#  Website: https://saezlab.github.io/omnipathr
+#  Git repo: https://github.com/saezlab/OmnipathR
+#
+
+
+#' Makes sure we have a string even if the argument was passed by NSE
+#'
+#' @importsFrom magrittr %>%
+#' @importsFrom rlang enquo quo_get_expr quo_text is_symbol
+.nse_ensure_str <- function(arg){
+
+    enquo(arg) %>%
+    {`if`(
+        is_symbol(quo_get_expr(.)),
+        quo_text(.),
+        quo_get_expr(.)
+    )}
+
+}
