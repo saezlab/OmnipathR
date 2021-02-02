a3c310ee2773f040e10420525ea34fc243a8be97
Türei Dénes <turei.denes@gmail.com>
2021-02-02 23:56:50 +0100

    cache stuff vol. 1

--- R/cache.R
+++ R/cache.R
@@ -22,17 +22,58 @@
 
 #' Sets up the cache once the module is loaded or after the cachedir option
 #' has been changed
+#'
+#' @importFrom rappdirs user_cache_dir
 omnipath_init_cache <- function(){
 
+    cachedir <- omnipath_default_cachedir()
+    omnipath_new_cachedir(cachedir)
+    options(omnipath.cachedir = cachedir)
+    logger::log_info('Initialized cache: `%s`.', cachedir)
 
+    invisible(cachedir)
+
+}
+
+
+#' Returns the default cache directory path
+#'
+#' @importFrom magrittr %>%
+omnipath_default_cachedir <- function(){
+
+    user_cache_dir() %>%
+    file.path('OmnipathR') %>%
+    normalizePath()
 
 }
 
 
 #' Sets up a new cache directory
-omnipath_new_cachedir <- function(){
+#'
+#' @importFrom jsonlite toJSON
+omnipath_new_cachedir <- function(path){
+
+    if(!omnipath_is_cachedir(path)){
+
+        logger::log_info('Setting up new cache directory `%s`.', path)
+        dir.create(path, showWarnings = FALSE, recursive = TRUE)
+        .omnipath_cache <<- list()
+        write(
+            jsonlite::toJSON(.omnipath_cache, pretty = TRUE),
+            file.path(path, 'cache.json')
+        )
+
+    }
+
+    invisible(path)
+
+}
 
 
+#' Tells if a directory looks like an OmnipathR cache directory
+omnipath_is_cachedir <- function(path){
+
+    dir.exists(path) && file.exists(file.path(cache, 'cache.json'))
 
 }
 
@@ -40,7 +81,15 @@ omnipath_new_cachedir <- function(){
 #' Determines the path of the cache directory according to the settings
 omnipath_get_cachedir <- function(){
 
+    cachedir <- options('omnipath.cachedir')
 
+    if(is.null(cachedir) || !omnipath_is_cachedir(cachedir)){
+
+        cachedir <- omnipath_init_cache()
+
+    }
+
+    return(cachedir)
 
 }
 
@@ -49,7 +98,14 @@ omnipath_get_cachedir <- function(){
 #' reading and writing of the cache file database
 omnipath_lock_cache_db <- function(){
 
+    lockfile <- omnipath_cache_lock_path()
 
+    for(i in 1:options('omnipath.cache_timeout')){
+
+        if(file.exists(lockfile))
+        Sys.sleep(1)
+
+    }
 
 }
 
@@ -61,10 +117,24 @@ omnipath_lock_cache_db <- function(){
 #' might not happen if the process crashes during such an operation. In
 #' this case you can manually call this function.
 #'
+#' @importFrom magrittr %>%
 #' @export
 omnipath_unlock_cache_db <- function(){
 
+    omnipath_cache_lock_path() %>%
+    file.remove()
 
+}
+
+
+#' Tells the path to the cache lock file
+#'
+#' @importFrom magrittr %>%
+omnipath_cache_lock_path <- function(){
+
+    'omnipath.cachedir' %>%
+    options() %>%
+    file.path('cache.lock')
 
 }
 
@@ -177,5 +247,30 @@ omnipath_cache_db_remove <- function(){
 omnipath_cache_db_edit <- function(){
 
 
+
+}
+
+
+#' Reads the cache DB contents from the disk to the memory
+#'
+#' @importFrom jsonlite
+omnipath_read_cache_db <- function(){
+
+
+
+}
+
+
+#' Writes the cache DB contents from the memory to the disk
+#'
+#' @importFrom jsonlite toJSON
+omnipath_write_cache_db <- function(){
+
+    omnipath_lock_cache_db()
+    write(
+        jsonlite::toJSON(.omnipath_cache, pretty = TRUE),
+        file.path()
+    )
+    omnipath_unlock_cache_db()
 
 }
