b778f42232e618db8fcc27d3e5c92e1727c3b748
Türei Dénes <turei.denes@gmail.com>
2021-02-15 15:33:18 +0100

    bma: coding style

--- R/bma.r
+++ R/bma.r
@@ -26,68 +26,94 @@
 #' Ends a function where something has gone wrong, printing information about the error
 #' @param a string with information about why the error occurred
 #' @NoRd
-wrongInput <- function(reason){
-    cat(reason)
+#' @importFrom logger log_warning
+wrong_input <- function(reason){
+    logger::log_warning(reason)
     return(NULL)
-    }
+}
 
 #' Returns a formatted string describing a BMA interaction between variables
-#' @param a unique id, variable ids describing the source and targets, and the edge description
+#'
+#' @param a unique id, variable ids describing the source and targets, and
+#' the edge description
 #' @NoRd
-bmaRelationship <- function(id,from,to,type){
-    rel <- sprintf('{"Id":%d,"FromVariable":%d,"ToVariable":%d,"Type":"%s"}', id, from, to, type)
+bma_relationship <- function(id, from, to, type){
+    rel <- sprintf(
+        '{"Id":%d, "FromVariable":%d, "ToVariable":%d, "Type":"%s"}',
+        id, from, to, type
+    )
     return(rel)
-    }
+}
 
-#' Returns a formatted string describing the model parameters of a BMA variable
-#' @param a unique id, human readable name (e.g. JAG1), unique variable id, granularity (number of levels) and the formula
+#' Returns a formatted string describing the model parameters of a BMA
+#' variable
+#'
+#' @param a unique id, human readable name (e.g. JAG1), unique variable id,
+#' granularity (number of levels) and the formula
 #' @NoRd
-bmaVariableModel <- function(id,name,granularity,formula=""){
-    var <- sprintf('{"Name":"%s","Id":%d,"RangeFrom":0,"RangeTo":%d,"Formula":"%s"}', name, id, granularity, formula)
+bma_variable_model <- function(id, name, granularity, formula = ""){
+    var <- sprintf(
+        '{"Name":"%s", "Id":%d, "RangeFrom":0, "RangeTo":%d, "Formula":"%s"}',
+        name, id, granularity, formula
+    )
     return(var)
-    }
+}
 
-#' Returns a formatted string describing the layout parameters of a BMA variable
-#' @param a unique id, human readable name (e.g. JAG1), granularity (number of levels) and the update formula
+#' Returns a formatted string describing the layout parameters of a BMA
+#' variable
+#'
+#' @param a unique id, human readable name (e.g. JAG1), granularity (number
+#' of levels) and the update formula
 #' @NoRd
-bmaVariableLayout <- function(id,name,x,y,description="") {
-    var <- sprintf('{"Id":%d,"Name":"%s","Type":"Constant","ContainerId":0,"PositionX":%f,"PositionY":%f,"CellX":0,"CellY":0,"Angle":0,"Description":"%s"}',id,name,x,y,description)
+bma_variable_layout <- function(id, name, x, y, description = "") {
+    var <- sprintf(
+        paste0(
+            '{"Id":%d, "Name":"%s", "Type":"Constant", "ContainerId":0, ',
+            '"PositionX":%f, "PositionY":%f, "CellX":0, "CellY":0, ',
+            '"Angle":0, "Description":"%s"}'
+        ),
+        id, name, x, y, description
+    )
     return(var)
-    }
+}
 
 #' Returns a string containing the target function of a variable
-#' 
-#' Returns either empty string (interpreted as default function), or granularity - activity of upstream inhibitor
-#' @param bool stating whether the interaciton is an inhibition, granularity of 
-#' variables (number of levels), and source of interaction
+#'
+#' @return Returns either empty string (interpreted as default function), or
+#' ranularity - activity of upstream inhibitor
+#' @param bool stating whether the interaciton is an inhibition, granularity
+#' of variables (number of levels), and source of interaction
 #' @NoRd
-bmaFormula <- function(inhibitor,granularity,upstream){
-    f <- ifelse(inhibitor,sprintf("%d-var(%s)",granularity,upstream),"")
+bma_formula <- function(inhibitor, granularity, upstream){
+    f <- ifelse(inhibitor, sprintf("%d-var(%s)", granularity, upstream), "")
     return(f)
 }
 
 #' Returns a string describing the evidence behind an interaction
-#' 
+#'
 #' Contains all interaction types with a simple descriptor and PMIDs
-#' @param takes an edge from omnipath "e", and optionally the name of the upstream variable ("incoming")
+#' @param takes an edge from omnipath "e", and optionally the name of the
+#' upstream variable ("incoming")
 #' @NoRd
-bmaDescription <- function(e,incoming=""){
+bma_description <- function(e, incoming = ""){
     sign <- ifelse(e$is_stimulation == 1,
-            ifelse(e$is_inhibition == 1,"Mixed","Activator"),
-            ifelse(e$is_inhibition == 1,"Inhibitor","Unknown"))
-    refs <- paste(unlist(e$references), sep = '', collapse = ',')
-    incoming <- ifelse(incoming == "","",paste("",incoming,"",sep=" "))
-    return(sprintf("%s%s-PMID:%s.",incoming,sign,refs))
+            ifelse(e$is_inhibition == 1, "Mixed", "Activator"),
+            ifelse(e$is_inhibition == 1, "Inhibitor", "Unknown"))
+    refs <- paste(unlist(e$references), sep = '', collapse = ', ')
+    incoming <- ifelse(incoming == "", "", paste("", incoming, "", sep = " "))
+    return(sprintf("%s%s-PMID:%s.", incoming, sign, refs))
     }
 
-#' Prints a BMA motif to the screen from a sequence of edges, which can be copy/pasted into the BMA canvas 
-#' 
+#' Prints a BMA motif to the screen from a sequence of edges, which can be
+#' copy/pasted into the BMA canvas
+#'
 #' Intended to parallel print_path_es
 #' @param takes an sequence of edges, a graph, and a granularity
 #' @export
-bmaMotif_es <- function(edgeSeq,G,granularity=2){
-    if(length(edgeSeq) == 0) {
-        wrongInput("\nempty path\n")
+#' @importFrom igraph tail_of head_of
+bma_motif_es <- function(edge_seq, G, granularity = 2){
+    if(length(edge_seq) == 0) {
+        wrong_input("BMA motif: empty path")
     }
     #Process-
     ## Create list of variables
@@ -95,47 +121,61 @@ bmaMotif_es <- function(edgeSeq,G,granularity=2){
     ## Create list of links
     ## Print format as follows (x is a string, xN is an integer, xF is a float)
     ### {"Model":     {"Name": "Omnipath motif",
-    ###              "Variables": [{"Name":"x","Id":xN,"RangeFrom"=0,"RangeTo"=granularity},...]
-    ###             "Relationships": [{"Id":xN,"FromVariable":xN,"ToVariable":xN,"Type":"Activator"},...]
+    ###              "Variables": [{"Name":"x", "Id":xN, "RangeFrom" = 0,
+    ###                             "RangeTo" = granularity}, ...]
+    ###             "Relationships": [{"Id":xN, "FromVariable":xN,
+    ###                            "ToVariable":xN, "Type":"Activator"}, ...]
     ###         }
-    ###  "Layout":     {"Variables": [{"Id":xN,"Name":"x","Type":"Constant","ContainerId":0,"PositionX":xF,"PositionY":xF,"CellX":0,"CellY":0,"Angle":0,"Description":""},...]
+    ###  "Layout":     {"Variables": [{"Id":xN, "Name":"x", "Type":"Constant",
+    ###                                "ContainerId":0, "PositionX":xF,
+    ###                                "PositionY":xF, "CellX":0, "CellY":0,
+    ###                                "Angle":0, "Description":""}, ...]
     ###            "Containers":[]
     ###            }
     ### }
 
     #Code for identifying sign
-    #signs <- ifelse(edgeSeq$is_stimulation == 1,
-    #    ifelse(edgeSeq$is_inhibition == 1,"(+/-)","( + )"),
-    #    ifelse(edgeSeq$is_inhibition == 1,"( - )","( ? )"))
-    #interaction <- paste0(" == ", signs," == >")
+    #signs <- ifelse(edge_seq$is_stimulation == 1,
+    #    ifelse(edge_seq$is_inhibition == 1, "( + /-)", "( + )"),
+    #    ifelse(edge_seq$is_inhibition == 1, "( - )", "( ? )"))
+    #interaction <- paste0(" == ", signs, " == >")
 
-    #relationships <- ifelse(edgeSeq$is_stimulation == 1,
-    #    ifelse(edgeSeq$is_inhibition == 1,"Activator","Activator"),
-    #    ifelse(edgeSeq$is_inhibition == 1,"Inhibitor",return(wrongInput("\nUnsigned input graph\n")))
-    sources <- tail_of(G, edgeSeq)$name
-    variableNames <- c(sources,head_of(G, edgeSeq)$name[length(edgeSeq)]) 
-    varNum <- length(variableNames)
+    #relationships <- ifelse(edge_seq$is_stimulation == 1,
+    #    ifelse(edge_seq$is_inhibition == 1, "Activator", "Activator"),
+    #    ifelse(edge_seq$is_inhibition == 1, "Inhibitor",
+    #    return(wrongInput("\nUnsigned input graph\n")))
+    sources <- tail_of(G, edge_seq)$name
+    variable_names <- c(sources, head_of(G, edge_seq)$name[length(edge_seq)])
+    var_num <- length(variable_names)
 
-    positions <- vector('list',varNum)
-    variables <- vector('list',varNum)
-    relationships <- vector('list',varNum-1)
+    positions <- vector('list', var_num)
+    variables <- vector('list', var_num)
+    relationships <- vector('list', var_num - 1)
 
 
     formula = ""
     description = ""
-    x=125
-    y=140
-    for (i in seq_along(variableNames))
+    x = 125
+    y = 140
+    for (i in seq_along(variable_names))
         {
-    v <- bmaVariableModel(i,variableNames[i],granularity,formula)
-    p <- bmaVariableLayout(i,variableNames[i],x,y,description)
-    if (i < varNum){
-        #Simplified sign- if inhibition, inhibitor, else (activator/mixed/unknown) activation
-        r <- bmaRelationship(i+varNum,i,i+1,ifelse(edgeSeq[i]$is_inhibition == 1,"Inhibitor","Activator"))
+    v <- bma_variable_model(i, variable_names[i], granularity, formula)
+    p <- bma_variable_layout(i, variable_names[i], x, y, description)
+    if (i < var_num){
+        # Simplified sign- if inhibition, inhibitor,
+        # else (activator/mixed/unknown) activation
+        r <- bma_relationship(
+            i + var_num, i, i + 1,
+            ifelse(edge_seq[i]$is_inhibition == 1, "Inhibitor", "Activator")
+        )
         relationships[[i]] <- r
-        formula <- bmaFormula((edgeSeq[i]$is_inhibition == 1),granularity,variableNames[i])
-        description <- bmaDescription(edgeSeq[i])
-        }
+        formula <- bma_formula(
+            (edge_seq[i]$is_inhibition == 1),
+            granularity,
+            variable_names[i]
+        )
+        description <- bma_description(edge_seq[i])
+    }
     positions[[i]] <- p
     variables[[i]] <- v
 
@@ -143,8 +183,17 @@ bmaMotif_es <- function(edgeSeq,G,granularity=2){
     ymod <- ifelse(i %% 2 == 0, 50, -50)
     y <- y + ymod
     }
-   result <- sprintf('{"Model": {"Name": "Omnipath motif","Variables":[%s],"Relationships":[%s]},"Layout":{"Variables":[%s],"Containers":[]}}\n',paste(variables, sep = '', collapse = ','),paste(relationships, sep = '', collapse = ','),paste(positions, sep = '', collapse = ','))
-   cat(result)
+    result <- sprintf(
+        paste0(
+            '{"Model": {"Name": "Omnipath motif", "Variables":[%s], '
+            '"Relationships":[%s]}, "Layout":{"Variables":[%s], '
+            '"Containers":[]}}\n'
+        ),
+        paste(variables, sep = '', collapse = ', '),
+        paste(relationships, sep = '', collapse = ', '),
+        paste(positions, sep = '', collapse = ', ')
+    )
+    cat(result)
 
 }
 
@@ -153,21 +202,27 @@ bmaMotif_es <- function(edgeSeq,G,granularity=2){
 #' Intended to parallel print_path_vs
 #' @param takes an sequence of nodes, and a granularity
 #' @export
-bmaMotif_vs <- function(nodeSeq,G){
+#' @importFrom logger log_warning
+#' @importFrom igraph E
+bma_motif_vs <- function(node_seq, G){
 
-    if(length(nodeSeq) == 0){
-        print("empty path")
+    if(length(node_seq) == 0){
+        logger::log_warning("BMA motif: empty path")
         return(invisible(NULL))
     }
-    nodeSeq_names <- unique_nodeSeq(nodeSeq)
-    for(i in seq(nodeSeq_names)){
-        print(paste0("pathway ", i, ": ", 
-            paste(nodeSeq_names[[i]],collapse = " -> ")))
-        edgeSet <- c()
-        for(j in 2:length(nodeSeq_names[[i]])){
-            edgeSet <- c(edgeSet, E(G)[nodeSeq_names[[i]][[j-1]]  %->%
-                nodeSeq_names[[i]][[j]]])
+    node_seq_names <- unique_node_seq(node_seq)
+    for(i in seq(node_seq_names)){
+        print(
+            paste0(
+                "pathway ", i, ": ",
+                paste(node_seq_names[[i]], collapse = " -> ")
+            )
+        )
+        edge_set <- c()
+        for(j in 2:length(node_seq_names[[i]])){
+            edge_set <- c(edge_set, E(G)[node_seq_names[[i]][[j-1]]  %->%
+                node_seq_names[[i]][[j]]])
         }
-        bmaMotif_es(E(G)[edgeSet],G)
+        bma_motif_es(E(G)[edge_set], G)
     }
 }
