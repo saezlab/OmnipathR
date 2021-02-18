acd3e850d7a1758bc4e7351db887449f9774d1e1
Türei Dénes <turei.denes@gmail.com>
2021-02-18 01:29:48 +0100

    caching vol 6

--- R/cache.R
+++ R/cache.R
@@ -20,13 +20,17 @@
 #
 
 
-.cache_status <- list(
-    UNKNOWN <- 'unknown',
-    STARTED <- 'started',
-    READY <- 'ready',
-    FAILED <- 'failed',
-    DELETED <- 'deleted'
+CACHE_STATUS <- list(
+    UNKNOWN = 'unknown',
+    STARTED = 'started',
+    READY = 'ready',
+    FAILED = 'failed',
+    DELETED = 'deleted'
 )
+
+BEGINNING_OF_TIME <- as.POSIXct(0, origin = '1970-01-01')
+
+PROTECTED_FILES <- c('cache.lock', 'cache.json')
 
 #' Sets up the cache once the module is loaded or after the cachedir option
 #' has been changed
@@ -188,8 +192,23 @@ omnipath_locked_cache_error <- function(){
 
 
 #' Searches for cache items
-omnipath_cache_search <- function(){
+#'
+#' Searches the cache records by matching the URL against a string or regexp.
+#'
+#' @return List of cache records matching the pattern.
+#'
+#' @param pattern String or regular expression.
+#' ... Passed to \code{\link{grep}}
+#'
+#' @importFrom purrr map_chr
+#' @export
+omnipath_cache_search <- function(pattern, ...){
 
+    .omnipath_cache %>%
+    map_chr('url') %>%
+    grep(pattern = pattern, ...) %>%
+    `[`(names(.omnipath_cache), .) %>%
+    `[`(.omnipath_cache, .)
 
 
 }
@@ -200,32 +219,233 @@ omnipath_cache_search <- function(){
 #' According to the parameters, it can remove contents older than a certain
 #' age, or contents having a more recent version, one specific item, or
 #' wipe the entire cache.
-omnipath_cache_remove <- function(){
+#'
+#' @param key The key of the cache record
+#' @param url URL pointing to the resource
+#' @param post HTTP POST parameters as a list
+#' @param payload HTTP data payload
+#' @param max_age Age of cache items in days. Remove everything that is older
+#' than this age
+#' @param min_age Age of cache items in days. Remove everything more recent
+#' than this age
+#' @param status Remove items having any of the states listed here
+#' @param keep_latest Keep the latest
+#' @param autoclean Remove the entries about failed downloads, the files in
+#' the cache directory which are missing from the cache database, and the
+#' entries without existing files in the cache directory
+#'
+#' @importFrom magrittr %<>% %>%
+#' @importFrom purrr map keep
+#' @export
+#' @seealso \code{\link{omnipath_cache_wipe}, \link{omnipath_cache_clean},
+#' \link{omnipath_cache_autoclean}}
+omnipath_cache_remove <- cache_locked %@% function(
+    key = NULL,
+    url = NULL,
+    post = NULL,
+    payload = NULL,
+    max_age = NULL,
+    min_age = NULL,
+    status = NULL,
+    keep_latest = FALSE,
+    wipe = FALSE,
+    autoclean = TRUE
+){
 
+    if(wipe){
 
+        .omnipath_cache <<- list()
+        omnipath_cache_clean()
+        return(invisible(NULL))
+
+    }
+
+    key <- ifelse(
+        is.null(key),
+        omnipath_cache_key(url = url, post = post, payload = payload),
+        key
+    )
+
+    .omnipath_cache %<>%
+    {`if`(
+        is.null(key),
+        .,
+        `if`(
+            (
+                is.null(max_age) &&
+                is.null(min_age) &&
+                is.null(status) &&
+                !keep_latest
+            ),
+            `[`(., .omnipath_cache %>% names %>% setdiff(key)),
+            `[`(., key)
+        )
+    )} %>%
+    map(
+        omnipath_cache_remove_versions,
+        max_age = max_age,
+        min_age = min_age,
+        status = status,
+        keep_latest = keep_latest
+    ) %>%
+    {`if`(
+        is.null(key),
+        .,
+        map(
+            .omnipath_cache,
+            function(record){
+                `if`(
+                    record$key %in% names(.),
+                    .[[record$key]],
+                    record
+                )
+            }
+        )
+    )}
+
+    .omnipath_cache <<- .omnipath_cache
+
+    if(autoclean){
+
+        omnipath_write_cache_db()
+        omnipath_unlock_cache_db()
+
+        omnipath_cache_clean_db()
+
+        omnipath_lock_cache_db()
+
+        omnipath_cache_autoclean()
+
+    }
 
 }
 
 
-#' Removes one item from the cache directory
-omnipath_cache_remove_one <- function(){
+#' Removes version items from a cache record
+#'
+#' @return A cache record with the version items removed
+#'
+#' @importFrom magrittr %<>% %>%
+omnipath_cache_remove_versions <- function(
+    record,
+    max_age = NULL,
+    min_age = NULL,
+    status = NULL,
+    keep_latest = FALSE
+){
+
+    record$versions %<>%
+        record$versions %>%
+        `[`(
+            omnipath_cache_filter_versions(
+                record = record,
+                latest = keep_latest,
+                max_age = max_age,
+                min_age = min_age,
+                status = setdiff(CACHE_STATUS, status)
+            )
+        )
+
+    invisible(record)
+
+}
 
 
+#' Removes the cache database entries without existing files
+#'
+#' @importFrom magrittr %>%
+#' @importFrom purrr map keep
+omnipath_cache_clean_db <- cache_locked %@% function(){
+
+    .omnipath_cache <<-
+        .omnipath_cache %>%
+        map(
+            ~keep(
+                .x$versions,
+                function(v){file.exists(v$path)}
+            )
+        ) %>%
+        keep(
+            function(record){length(record$versions) > 0}
+        )
+
+}
+
+
+#' Permanently removes all the cache contents
+#'
+#' After this operation the cache directory will be completely empty,
+#' except an empty cache database file.
+#'
+#' @export
+#' @importFrom magrittr %>%
+#' @seealso \code{\link{omnipath_cache_remove}}
+omnipath_cache_wipe <- cache_locked %@% function(){
+
+    omnipath_get_cachedir() %>%
+    list.files() %>%
+    setdiff(PROTECTED_FILES) %>%
+    file.path(omnipath_get_cachedir(), .) %>%
+    file.remove()
+
+    .omnipath_cache <<- list()
+    return(invisible(NULL))
 
 }
 
 
 #' Removes the items from the cache directory which are unknown by the cache
 #' database
+#'
+#' @importFrom purrr map_chr map
+#' @importFrom magrittr %>%
 omnipath_cache_clean <- function(){
 
 
+
+    files_in_db <-
+        .omnipath_cache %>%
+        map(~map_chr(.x$versions, 'path')) %>%
+        unlist() %>%
+        basename()
+
+    omnipath_get_cachedir() %>%
+    list.files() %>%
+    setdiff(files_in_db) %>%
+    setdiff(PROTECTED_FILES) %>%
+    file.path(omnipath_get_cachedir(), .) %>%
+    file.remove()
+
+    invisible(NULL)
+
+}
+
+
+#' Keeps only the latest versions of complete downloads
+#'
+#' Removes the old versions, the failed downloads and the files in the cache
+#' directory which are missing from the database. For more flexible
+#' operations use \link{\code{omnipath_cache_remove}} and
+#' \link{\code{omnipath_cache_clean}}.
+#'
+#' @export
+#'
+#' Removes the items from the cache directory which are unknown by the cache
+#' database
+omnipath_cache_autoclean <- function(){
+
+    omnipath_cache_remove(
+        keep_most_recent = TRUE,
+        status = CACHE_STATUS$READY
+    )
+    omnipath_cache_clean()
 
 }
 
 
 #' Retrieves one item from the cache directory
 #'
+#' @param key The key of the cache record
 #' @param url URL pointing to the resource
 #' @param post HTTP POST parameters as a list
 #' @param payload HTTP data payload
@@ -322,7 +542,7 @@ omnipath_cache_load <- function(
         (
             is.null(version) ||
             !(version %in% record$versions) ||
-            !(record$versions[[version]]$status == .cache_status$READY)
+            !(record$versions[[version]]$status == CACHE_STATUS$READY)
         ),
         omnipath_cache_latest_version(record),
         version
@@ -377,7 +597,7 @@ omnipath_cache_save <- function(
     version <- omnipath_cache_update_status(
         key = record$key,
         version = version,
-        status = .cache_status$STARTED
+        status = CACHE_STATUS$STARTED
     )
 
     target_path <- .omnipath_cache[[key]]$versions[[version]]$path
@@ -419,7 +639,7 @@ omnipath_cache_move_in <- function(
     version <- omnipath_cache_update_status(
         key = key,
         version = version,
-        status = .cache_status$STARTED
+        status = CACHE_STATUS$STARTED
     )
 
     target_path <- .omnipath_cache[[key]]$versions[[version]]$path
@@ -536,10 +756,10 @@ omnipath_cache_new_version <- function(key, version = NULL){
         post = record$post,
         payload = record$payload,
         dl_started = NULL,
-        status = .cache_status$UNKNOWN
+        status = CACHE_STATUS$UNKNOWN
     )
 
-    .omnipath_cache[[key]]$versions <- merge.list(
+    .omnipath_cache[[key]]$versions <- RCurl::merge.list(
         .omnipath_cache[[key]]$versions,
         new_version_record$versions
     )
@@ -573,15 +793,97 @@ omnipath_cache_next_version <- function(key){
 
 #' Finds the most recent version in a cache record
 #'
-#' @return The version ID with the most recent download finished time
-#' @importFrom magrittr %<>%
-omnipath_cache_latest_version <- function(record, max_age = NULL){
+#' @return Character: the version ID with the most recent download finished
+#' time
+#' @param record A cache record
+#'
+#' @export
+omnipath_cache_latest_version <- function(record){
 
-    versions <- names(record$versions)
+    omnipath_cache_filter_versions(record = record, latest = TRUE)
+
+}
+
+#' Filters the versions from one cache record
+#'
+#' Filters the versions based on multiple conditions: their age and status
+#'
+#' @return Character vector with version IDs, NA if no version satisfies the
+#' conditions
+#' @param record A cache record
+#' @param latest Return the most recent version
+#' @param max_age The maximum age in days (e.g. 5: 5 days old or more recent)
+#' @param min_age The minimum age in days (e.g. 5: 5 days old or older)
+#' @param status Character vector with status codes. By default only the
+#' versions with `ready` (completed download) status are selected
+#'
+#' @importFrom magrittr %<>% %>%
+#' @importFrom purrr map map_chr
+#' @export
+omnipath_cache_filter_versions <- function(
+    record,
+    latest = FALSE,
+    max_age = NULL,
+    min_age = NULL,
+    status = CACHE_STATUS$READY
+){
+
+    days_ago <- function(days){
+
+        as.POSIXct(days * 24 * 3600 * -1, origin = Sys.time())
+
+    }
+
+    version_ids <- names(record$versions)
+    versions <- record$versions
+    statuses <- map_chr(versions, 'status')
+    selection <- statuses %in% status
+
+    if(!is.null(max_age)){
+        max_age <- days_ago(max_age)
+        selection %<>% `&`(which_dl_finished(versions, max_age))
+    }
+
+    if(!is.null(min_age)){
+        min_age <- days_ago(min_age)
+        selection %<>% `&`(which_dl_finished(versions, max_age, op = `<=`))
+    }
+
+    if(latest){
+        t_latest <- versions %>%
+            map(function(v){v$dl_finished}) %>%
+            unlist()
+        selection %<>% `&`(which_dl_finished, t_latest, op = `==`)
+    }
+
+
     dates <- map(record$versions, function(v){v$dl_finished})
     versions <- versions[!sapply(dates, is.null)]
     dates %<>% unlist
     return(versions[which.max(dates)])
+
+}
+
+
+#' Selects the versions which fit a time threshold
+#'
+#' @param versions List of version entries
+#' @param t Timestamp (POSIXct object)
+#' @param op Operator, e.g. `>=` for larger than (more recent) or `<=` (older)
+#'
+#' @return Logical vector same length as `versions`
+#'
+#' @importFrom purrr map_lgl
+which_dl_finished <- function(versions, t, op = `>=`){
+
+    map_lgl(
+        versions,
+        function(v){
+            !is.null(v$dl_finished) &&
+            op(v$dl_finished, t)
+        }
+    )
+
 }
 
 
@@ -668,7 +970,7 @@ omnipath_cache_record <- function(
         post = NULL,
         payload = NULL,
         dl_started = Sys.time(),
-        status = .cache_status$STARTED
+        status = CACHE_STATUS$STARTED
     ){
 
     version %<>% as.character
@@ -707,12 +1009,70 @@ omnipath_cache_record <- function(
 
 #' Generates a hash which identifies an element in the cache database
 #'
+#' @param url Character vector with URLs
+#' @param post List with the HTTP POST parameters or a list of lists if
+#' the url vector is longer than 1. NULL for queries without POST parameters.
+#' @param payload HTTP data payload. List with multiple items if the url
+#' vector is longer than 1. NULL for queries without data.
+#'
+#' @return Character vector of cache record keys
+#'
 #' @importFrom digest sha1_digest
 #' @importFrom magrittr %>%
+#' @export
 omnipath_cache_key <- function(url, post = NULL, payload = NULL){
 
-    list(url, post, payload) %>%
-    sha1_digest()
+    url %>%
+    omnipath_url_to_list(post, payload) %>%
+    pmap_chr(sha1_digest)
+
+}
+
+
+#' Makes sure the post and the payload is the same length as url
+#'
+#' @param url Character vector with URLs
+#' @param post List with the HTTP POST parameters or a list of lists if
+#' the url vector is longer than 1. NULL for queries without POST parameters.
+#' @param payload HTTP data payload. List with multiple items if the url
+#' vector is longer than 1. NULL for queries without data.
+#'
+#' @return List of 3 lists or vectors: url, post and payload
+#' @importFrom magrittr %<>% %>%
+#' @importFrom purrr pmap_chr map map_lgl
+omnipath_url_to_list <- function(url, post = NULL, payload = NULL){
+
+    null_list <- function(value){
+
+        # if the url is a character vector of length 1
+        # and the post is a list of parameters
+        if(
+            length(url) == 1 &&
+            is.list(value) &&
+            value %>% map_lgl(is.list) %>% any %>% `!`
+        ){
+            value %<>% list
+        }
+
+        `if`(
+            is.null(value),
+            url %>% length %>% seq %>% map(function(x){NULL}),
+            value
+        )
+
+    }
+
+    if(length(url) != length(post) && !is.null(post)){
+        post %<>% list
+    }
+    if(length(url) != length(payload) && !is.null(post)){
+        post %<>% list
+    }
+
+    post %<>% null_list
+    payload %<>% null_list
+
+    list(url, post, payload)
 
 }
 
