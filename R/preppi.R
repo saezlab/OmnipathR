8aef89ec7618db65d359e11fa552bc4aa11daadd
Türei Dénes <turei.denes@gmail.com>
2021-06-13 22:16:12 +0200

    new resource: PrePPI

---
+++ R/preppi.R
@@ -0,0 +1,57 @@
+#!/usr/bin/env Rscript
+
+#
+#  This file is part of the `OmnipathR` R package
+#
+#  Copyright
+#  2018-2021
+#  Saez Lab, Uniklinik RWTH Aachen, Heidelberg University
+#
+#  File author(s): Alberto Valdeolivas
+#                  Dénes Türei (turei.denes@gmail.com)
+#                  Attila Gábor
+#
+#  Distributed under the MIT (Expat) License.
+#  See accompanying file `LICENSE` or find a copy at
+#      https://directory.fsf.org/wiki/License:Expat
+#
+#  Website: https://saezlab.github.io/omnipathr
+#  Git repo: https://github.com/saezlab/OmnipathR
+#
+
+
+#' Interactions from PrePPI
+#'
+#' Retrieves predicted protein-protein interactions from the PrePPI
+#' database (\url{http://honig.c2b2.columbia.edu/preppi}). The interactions
+#' in this table are supposed to be correct with a > 0.5 probability.
+#'
+#' @param ... Minimum values for the scores. The available scores are:
+#'     str, protpep, str_max, red, ort, phy, coexp, go, total, exp and final.
+#'
+#' @return A data frame (tibble) of interactions with scores, databases
+#'     and literature references.
+#'
+#' @examples
+#' preppi <- preppi_download()
+#'
+#' @importFrom magrittr %>% %T>%
+#' @importFrom readr read_tsv cols
+#' @importFrom dplyr filter
+#' @importFrom rlang !!!
+#' @export
+preppi_download <- function(...){
+
+    'preppi' %>%
+    archive_extractor(
+        path = 'preppi_final600.txt',
+        reader = read_tsv,
+        reader_param = list(
+            col_types = cols(),
+            progress = FALSE
+        ),
+        resource = 'PrePPI'
+    ) %T>%
+    load_success()
+
+}
