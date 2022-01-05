828e18fab8ddbbe5a8829134787d3ef5f9b44ba4
deeenes <turei.denes@gmail.com>
2022-01-05 22:07:43 +0100

    2021 -> 2022 in file headers

--- R/pathwaycommons.R
+++ R/pathwaycommons.R
@@ -4,7 +4,7 @@
 #  This file is part of the `OmnipathR` R package
 #
 #  Copyright
-#  2018-2021
+#  2018-2022
 #  Saez Lab, Uniklinik RWTH Aachen, Heidelberg University
 #
 #  File author(s): Alberto Valdeolivas
