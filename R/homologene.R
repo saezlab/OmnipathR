f2beeef696de16806f8cb37a21a15fdce4a0514d
deeenes <turei.denes@gmail.com>
2022-05-14 14:41:57 +0200

    `homologene_download`: orthology table for a pair of organisms

--- R/homologene.R
+++ R/homologene.R
@@ -24,7 +24,9 @@
 #'
 #' Retrieves NCBI HomoloGene data without any processing. Processed tables
 #' are more useful for most purposes, see below other functions that provide
-#' those.
+#' those. Genes of various organisms are grouped into homology groups
+#' ("hgroup" column). Organisms are identified by NCBI Taxonomy IDs, genes
+#' are identified by four different identifier types.
 #'
 #' @return A data frame as provided by NCBI HomoloGene.
 #'
@@ -32,20 +34,27 @@
 #' hg <- homologene_raw()
 #' hg
 #' # # A tibble: 275,237 × 6
-#' #    hgroup ncbi_taxid  entrez genesymbol             gi refseqp
-#' #     <dbl>      <dbl>   <dbl> <chr>               <dbl> <chr>
-#' #  1      3       9606      34 ACADM             4557231 NP_000007.1
-#' #  2      3       9598  469356 ACADM           160961497 NP_001104286.1
-#' #  3      3       9544  705168 ACADM           109008502 XP_001101274.1
-#' #  4      3       9615  490207 ACADM           545503811 XP_005622188.1
-#' #  5      3       9913  505968 ACADM           115497690 NP_001068703.1
+#' #    hgroup ncbi_taxid entrez  genesymbol  gi        refseqp
+#' #     <int>      <int> <chr>   <chr>       <chr>     <chr>
+#' #  1      3       9606 34      ACADM       4557231   NP_000007.1
+#' #  2      3       9598 469356  ACADM       160961497 NP_001104286.1
+#' #  3      3       9544 705168  ACADM       109008502 XP_001101274.1
+#' #  4      3       9615 490207  ACADM       545503811 XP_005622188.1
+#' #  5      3       9913 505968  ACADM       115497690 NP_001068703.1
 #' # # . with 275,232 more rows
 #'
-#' @importFrom magrittr %>%
-#' @importFrom readr cols
+#' # which organisms are available?
+#' common_name(unique(hg$ncbi_taxid))
+#' #  [1] "Human" "Chimpanzee" "Macaque" "Dog" "Cow" "Mouse" "Rat" "Zebrafish"
+#' #  [9] "D. melanogaster" "Caenorhabditis elegans (PRJNA13758)"
+#' # [11] "Tropical clawed frog" "Chicken"
+#' # ...and 9 more organisms with missing English names.
+#'
+#' @importFrom magrittr %>% %T>%
+#' @importFrom readr cols col_integer col_character
 #' @export
 #' @seealso \itemize{
-#'     \item{\code{\link{}}}
+#'     \item{\code{\link{homologene_download}}}
 #' }
 homologene_raw <- function(){
 
@@ -55,10 +64,72 @@ homologene_raw <- function(){
     generic_downloader(
         reader_param = list(
             col_names = hdr,
-            col_types = cols()
+            col_types = cols(
+                hgroup = col_integer(),
+                ncbi_taxid = col_integer(),
+                entrez = col_character(),
+                gi = col_character()
+            )
         ),
         resource = 'NCBI HomoloGene'
     ) %T>%
     load_success()
+
+}
+
+
+#' Orthology table for a pair of organisms
+#'
+#' Orthologous pairs of genes for a pair of organisms from NCBI HomoloGene,
+#' using one identifier type.
+#'
+#' @param target Character or integer: name or ID of the target organism.
+#' @param source Character or integer: name or ID of the source organism.
+#' @param id_type Symbol or character: identifier type, possible values are
+#'     "genesymbol", "entrez", "refseqp" or "gi".
+#'
+#' @details
+#' The operation of this function is symmetric, *source* and *target* are
+#' interchangeable but determine the column layout of the output. The column
+#' "hgroup" is a numberic identifier of the homology groups. Most of the
+#' groups consist of one pair of orthologous genes (one-to-one mapping), and
+#' a few of them multiple ones (one-to-many or many-to-many mappings).
+#'
+#' @return A data frame with orthologous identifiers between the two organisms.
+#'
+#' @examples
+#' chimp_human <- homologene_download(chimpanzee, human, refseqp)
+#' chimp_human
+#' # # A tibble: 17,737 × 3
+#' #    hgroup refseqp_source refseqp_target
+#' #     <int> <chr>          <chr>
+#' #  1      3 NP_000007.1    NP_001104286.1
+#' #  2      5 NP_000009.1    XP_003315394.1
+#' #  3      6 NP_000010.1    XP_508738.2
+#' #  4      7 NP_001096.1    XP_001145316.1
+#' #  5      9 NP_000014.1    XP_523792.2
+#' # # . with 17,732 more rows
+#'
+#' @importFrom rlang !! enquo sym
+#' @importFrom magrittr %>%
+#' @importFrom dplyr inner_join filter select
+#' @export
+homologene_download <- function(
+    target = 10090L,
+    source = 9606L,
+    id_type = 'genesymbol'
+){
+
+    source <- .nse_ensure_str(!!enquo(source)) %>% ncbi_taxid
+    target <- .nse_ensure_str(!!enquo(target)) %>% ncbi_taxid
+    id_type <- .nse_ensure_str(!!enquo(id_type))
+
+    homologene_raw() %>%
+    {inner_join(
+        filter(., ncbi_taxid == source) %>% select(hgroup, !!sym(id_type)),
+        filter(., ncbi_taxid == target) %>% select(hgroup, !!sym(id_type)),
+        by = 'hgroup',
+        suffix = c('_source', '_target')
+    )}
 
 }
