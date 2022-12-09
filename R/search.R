76838e4778b7bc47cae9ac9e22a6aff1ef9c2cfa
deeenes <turei.denes@gmail.com>
2022-12-10 00:16:55 +0100

    fixed all trivial issues with search index build

--- R/search.R
+++ R/search.R
@@ -20,9 +20,52 @@
 #
 
 
-build_search_index <- function() {
+#' Search index for OmniPath: load or build
+#'
+#' @param rebuild Logical: rebuild the index even if it could be loaded
+#'     from the cache.
+#'
+#' @return Data frame of six columns: resource, dataset, key, value, where,
+#'     database.
+#'
+#' @importFrom magrittr %>%
+#' @noRd
+build_index <- function(rebuild = FALSE) {
+
+    `if`(rebuild, NULL, omnipath_cache_load('omnipath_search_index')) %>%
+    if_null(build_index_impl())
+
+}
+
+
+#' Search index for OmniPath
+#'
+#' @return Data frame of six columns: resource, dataset, key, value, where,
+#'     database.
+#'
+#' @importFrom purrr keep map
+#' @importFrom dplyr mutate bind_rows
+#' @importFrom stringr str_to_lower str_extract
+#' @importFrom logger log_success
+#' @noRd
+build_index_impl <- function() {
 
     log_success('Building search index, this may take a few minutes.')
+
+    getNamespace('OmnipathR') %>%
+    names %>%
+    keep(endsWith, '_search_index') %>%
+    map(
+        function(index_builder){
+            get(index_builder)() %>%
+            mutate(database = str_extract(index_builder, '[^_]+'))
+        }
+    ) %>%
+    bind_rows() %>%
+    mutate(value_lower = str_to_lower(value)) %>%
+    omnipath_cache_save(url = 'omnipath_search_index')
+
+    log_success('Finished building search index.')
 
 }
 
@@ -34,22 +77,25 @@ build_search_index <- function() {
 #' @importFrom tidyr unnest
 #' @importFrom magrittr %>%
 #' @importFrom dplyr bind_rows select distinct rename mutate
+#' @importFrom logger log_info
 #' @noRd
 annotations_search_index <- function() {
 
     # NSE vs R CMD check workaround
     value <- label <- source <- resource <- NULL
 
+    log_info('Building search index for the annotations database.')
+
     database_summary('annotations', return_df = TRUE) %>%
     unnest('value') %>%
     rename(resource = source) %>%
-    mutate(dataset = NA, key = NA) %>%
+    mutate(dataset = NA) %>%
     bind_rows(
 
         select(., -value) %>%
         distinct %>%
         rename(value = label) %>%
-        mutate(where = 'field_name'),
+        mutate(where = 'field_name', key = NA),
 
         rename(., key = label) %>%
         mutate(where = 'field_value')
@@ -65,15 +111,18 @@ annotations_search_index <- function() {
 #'
 #' @importFrom magrittr %>%
 #' @importFrom dplyr rename bind_rows mutate distinct select
+#' @importFrom logger log_info
 #' @noRd
 intercell_search_index <- function() {
 
     # NSE vs R CMD check workaround
     database <- category <- parent <- resource <- NULL
 
+    log_info('Building search index for the intercell database.')
+
     database_summary('intercell', return_df = TRUE) %>%
     rename(resource = database) %>%
-    mutate(dataset = NA, key = NA) %>%
+    mutate(dataset = NA) %>%
     bind_rows(
 
         rename(., key = parent, value = category) %>%
@@ -83,7 +132,7 @@ intercell_search_index <- function() {
         select(., -category) %>%
         rename(value = parent) %>%
         distinct %>%
-        mutate(where = 'parent_category')
+        mutate(where = 'parent_category', key = NA)
 
     )
 
@@ -97,11 +146,14 @@ intercell_search_index <- function() {
 #' @importFrom tidyr unnest_longer separate_rows separate
 #' @importFrom dplyr select rename bind_rows mutate distinct
 #' @importFrom stringr str_extract str_replace
+#' @importFrom logger log_info
 #' @noRd
 interactions_search_index <- function() {
 
     # NSE vs R CMD check workaround
     dataset <- datasets <- sources <- type <- resource <- NULL
+
+    log_info('Building search index for the interactions database.')
 
     import_all_interactions(fields = c('extra_attrs', 'datasets')) %>%
     datasets_one_column(remove_logicals = TRUE) %>%
@@ -164,8 +216,11 @@ interactions_search_index <- function() {
 #' @importFrom magrittr %>%
 #' @importFrom tidyr separate_rows
 #' @importFrom dplyr select filter mutate distinct
+#' @importFrom logger log_info
 #' @noRd
 complexes_search_index <- function() {
+
+    log_info('Building search index for the complexes database.')
 
     import_omnipath_complexes() %>%
     select(resource = sources, value = name) %>%
@@ -185,8 +240,11 @@ complexes_search_index <- function() {
 #' @importFrom tidyr separate_rows
 #' @importFrom dplyr select mutate distinct bind_rows
 #' @importFrom stringr str_extract str_replace
+#' @importFrom logger log_info
 #' @noRd
-complexes_search_index <- function() {
+enzsub_search_index <- function() {
+
+    log_info('Building search index for the enzyme-substrate database.')
 
     import_omnipath_enzsub() %>%
     select(resource = sources, value = modification) %>%
@@ -195,7 +253,7 @@ complexes_search_index <- function() {
     bind_rows(
 
         # key is NA, value is PTM type
-        mutate(dataset = NA, key = NA, where = 'ptm_type'),
+        mutate(., dataset = NA, key = NA, where = 'ptm_type'),
 
         # key is NA, value is secondary resource name
         mutate(
@@ -211,12 +269,31 @@ complexes_search_index <- function() {
 }
 
 
+#' Access the OmniPath search index
+#'
+#' @param rebuild Logical: rebuild the index even if it could be loaded
+#'     from the cache.
+#'
+#' @return Data frame of six columns: resource, dataset, key, value, where,
+#'     database.
+#'
+#' @noRd
+get_index <- function(rebuild = FALSE) {
+
+    get_db('search_index', param = list(rebuild = rebuild))
+
+}
+
+
 #' Search in OmniPath
+#'
+#' @param rebuild_index Logical: rebuild the index even if it could be loaded
+#'     from the cache.
 #'
 #' Search for keywords in the resource and attribute names and metadata.
 #'
-search_omnipath <- function(...) {
+search_omnipath <- function(..., rebuild_index = FALSE) {
 
-
+    index <- get_index(rebuild = rebuild_index)
 
 }
