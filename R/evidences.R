ff22cd1d05c6c5a40705e54c4f72a5d2ce2a0688
deeenes <turei.denes@gmail.com>
2023-05-21 19:28:45 +0200

    `unnest_evidences`: separate evidences by direction & effect, into new rows or columns

--- R/evidences.R
+++ R/evidences.R
@@ -19,20 +19,23 @@
 #  Git repo: https://github.com/saezlab/OmnipathR
 #
 
+EVIDENCES_KEYS <- c('positive', 'negative', 'directed', 'undirected')
 
-#' Converts the extra_attrs column from JSON encoded to list
+
+#' Converts the evidences column from JSON encoded to list
 #'
 #' @param data A data frame from the OmniPath web service.
+#' @param ... Passed to `jsonlite::fromJSON`.
 #'
-#' @return The input data frame with the extra_attrs column converted
+#' @return The input data frame with the "evidences" column converted
 #'     to list.
 #'
 #' @importFrom magrittr %>%
 #' @noRd
-deserialize_evidences <- function(data){
+deserialize_evidences <- function(data, ...){
 
     data %>%
-    deserialize_json_col('evidences')
+    deserialize_json_col('evidences', ...)
 
 }
 
@@ -46,3 +49,34 @@ has_evidences <- function(data){
     data %>% has_column('evidences')
 
 }
+
+
+#' Separate evidences by direction and effect sign
+#'
+#' @param data An interaction data frame with "evidences" column.
+#' @param longer Logical: If TRUE, the "evidences" column is split into rows.
+#'
+#' @return The data frame with new columns or new rows by direction and sign.
+#'
+#' @examples
+#' omnipath <- import_omnipath_interactions(fields = 'evidences')
+#' omnipath <- unnest_evidences(omnipath)
+#' colnames(omnipath)
+#'
+#' @importFrom magrittr %>% extract
+#' @importFrom dplyr mutate
+#' @importFrom purrr map
+#' @importFrom tidyr unnest_longer unnest_wider
+#' @importFrom rlang exec !!!
+#' @export
+unnest_evidences <- function(data, longer = FALSE) {
+
+    unnest_method <- `if`(longer, unnest_longer, unnest_wider)
+    unnest_args <- `if`(longer, list(indices_to = 'direction'), list())
+
+    data %>%
+    mutate(evidences = map(evidences, extract, EVIDENCES_KEYS)) %>%
+    exec(unnest_method, ., 'evidences', !!!unnest_args)
+
+}
+
