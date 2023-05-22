7ac3b8188aac06a22c27a82a645f1481b79136a4
deeenes <turei.denes@gmail.com>
2023-05-22 06:37:16 +0200

    `only_from` & `filter_evidences`: reconstitute interaction records from evidences

--- R/evidences.R
+++ R/evidences.R
@@ -42,11 +42,24 @@ deserialize_evidences <- function(data, ...){
 
 #' Tells if a data frame has a column "evidences"
 #'
-#' @importFrom magrittr %>%
+#' @importFrom magrittr %>% or
 #' @noRd
-has_evidences <- function(data){
+has_evidences <- function(data, wide_ok = FALSE){
 
-    data %>% has_column('evidences')
+    data %>%
+    has_column('evidences') %>%
+    or(wide_ok && has_evidences_wide(data))
+
+}
+
+
+#' Tells if a data frame has evidence columns by direction and sign
+#'
+#' @importFrom magrittr %>% is_in
+#' @noRd
+has_evidences_wide <- function(data) {
+
+    EVIDENCES_KEYS %>% is_in(colnames(data)) %>% all
 
 }
 
@@ -55,9 +68,9 @@ has_evidences <- function(data){
 #'
 #' @importFrom magrittr %>% extract
 #' @noRd
-must_have_evidences <- function(data, env = parent.frame()){
+must_have_evidences <- function(data, wide_ok = FALSE, env = parent.frame()){
 
-    if(!has_evidences(data)) {
+    if(!has_evidences(data, wide_ok = wide_ok)) {
 
         parent_call <- sys.call(-1L)
 
@@ -107,3 +120,250 @@ unnest_evidences <- function(data, longer = FALSE) {
 
 }
 
+
+#' Filter evidences by dataset and resource
+#'
+#' @param data An interaction data frame with some columns containing evidences
+#'      as nested lists.
+#' @param ... The evidences columns to filter: tidyselect syntax is supported.
+#'      By default the columns "evidences", "positive", "negative", "directed"
+#'      and "undirected" are filtered, if present.
+#' @param datasets A character vector of dataset names.
+#' @param resources A character vector of resource names.
+#'
+#' @return The input data frame with the evidences in the selected columns
+#'    filtered.
+#'
+#' @importFrom magrittr %>%
+#' @importFrom rlang expr
+#' @importFrom tidyselect eval_select
+#' @importFrom dplyr mutate across
+#' @export
+filter_evidences <- function(data, ..., datasets = NULL, resources = NULL) {
+
+    columns <-
+        expr(...) %>%
+        eval_select(data) %>%
+        names %>%
+        if_null_len0(
+            EVIDENCES_KEYS %>%
+            c('evidences') %>%
+            intersect(colnames(data))
+        )
+
+    data %>%
+    mutate(across(columns, ~filter_evs_lst(.x, datasets, resources, columns)))
+
+}
+
+
+#' Filter evidences within a nested list
+#'
+#' @importFrom magrittr %>% is_in not
+#' @importFrom purrr map2 keep map
+#' @noRd
+filter_evs_lst <- function(lst, datasets, resources, columns) {
+
+    filter_evs <- function(x) {
+
+        x %>%
+        {`if`(
+            names(.) %>% is.null %>% not,
+            map2(
+                 names(.),
+                 .,
+                 function(name, value) {
+                     `if`(
+                          name %>% is_in(EVIDENCES_KEYS),
+                          value %>% filter_evs,
+                          value
+                     )
+                 }
+            ),
+            keep(
+                .,
+                function(ev) {
+                    (is.null(datasets) || ev$dataset %in% datasets) &&
+                    (
+                        is.null(resources) ||
+                        ev$resource %in% resources ||
+                        if_null(ev$via, '') %in% resources
+                    )
+                }
+            )
+        )}
+
+    }
+
+
+    lst %>%
+    map(filter_evs)
+
+}
+
+
+#' Recreate interaction data frame based on certain datasets and resources
+#'
+#' @param data An interaction data frame from the OmniPath web service with
+#'     evidences column.
+#' @param datasets Character: a vector of dataset labels. Only evidences from
+#'     these datasets will be used.
+#' @param resources Character: a vector of resource labels. Only evidences
+#'     from these resources will be used.
+#'
+#' @return A copy of the interaction data frame restricted to the given
+#'     datasets and resources.
+#'
+#' @details
+#' The OmniPath interactions database fully integrates all attributes from all
+#' resources for each interaction. This comes with the advantage that
+#' interaction data frames are ready for use in most of the applications;
+#' however, it makes it impossible to know which of the resources and
+#' references support the direction or effect sign of the interaction. This
+#' information can be recovered from the "evidences" column. The "evidences"
+#' column preserves all the details about interaction provenances. In cases
+#' when you want to use a faithful copy of a certain resource or dataset, this
+#' function will help you do so. Still, in most of the applications the best is
+#' to use the interaction data as it is returned by the web service.
+#'
+#' @examples
+#' ci <- collectri()
+#' ci <- only_from(ci, datasets = 'collectri')
+#'
+#' @importFrom magrittr %>%
+#' @importFrom rlang as_function
+#' @importFrom purrr keep
+#' @importFrom dplyr select
+#' @export
+only_from <- function(data, datasets = NULL, resources = NULL) {
+
+    if(is.null(datasets) && is.null(resources)) {
+        return(data)
+    }
+
+    must_have_evidences(data, wide_ok = TRUE)
+
+    has_wide <- data %>% has_evidences_wide
+
+    data %>%
+    {`if`(has_wide, ., unnest_evidences(.))} %>%
+    filter_evidences(datasets = datasets, resources = resources) %>%
+    from_evidences() %>%
+    {`if`(has_wide, ., select(-EVIDENCE_KEYS))}
+
+}
+
+
+#' Recreate interaction records from evidences columns
+#'
+#' @details
+#' The OmniPath interaction data frames specify interactions primarily by
+#' three columns: "is_directed", "is_stimulation" and "is_inhibition".
+#' Besides these, there are the "sources" and "references" columns that are
+#' always included in data frames created by OmnipathR and list the resources
+#' and literature references for each interaction, respectively. The optional
+#' "evidences" column is required to find out which of the resources and
+#' references support the direction or effect sign of the interaction. To
+#' properly recover information for arbitrary subsets of resources or
+#' datasets, the evidences can be filtered first, and then the standard
+#' data frame columns can be reconstructed from the selected evidences.
+#' This function is able to do the latter. It expects either an "evidences"
+#' column or evidences in their wide format 4 columns layout. It overwrites
+#' the standard columns of interaction records based on data extracted from
+#' the evidences.
+#'
+#' @importFrom stringr str_detect
+#' @noRd
+from_evidences <- function(data) {
+
+    must_have_evidences(data, wide_ok = TRUE)
+
+    prefix <- data$references[1L] %>% str_detect('^\\w+:')
+
+    data %>%
+    {`if`(has_evidences_wide(.), ., unnest_evidences(.))} %>%
+    mutate(
+        is_directed = as.integer(lgl_from(., positive, negative, directed)),
+        is_stimulation = as.integer(map_lgl(positive, ~length(.x) > 0L)),
+        is_inhibition = as.integer(map_lgl(negative, ~length(.x) > 0L)),
+        sources = resources_from(., EVIDENCES_KEYS),
+        references = references_from(., EVIDENCES_KEYS, prefix = prefix)
+    )
+
+}
+
+
+#' Logical vector from a set of list columns
+#'
+#' @importFrom magrittr %>%
+#' @importFrom dplyr select
+#' @importFrom purrr pmap_lgl
+#' @noRd
+lgl_from <- function(data, ...) {
+
+    data %>%
+    select(...) %>%
+    pmap_lgl(~length(c(...)) > 0L)
+
+}
+
+
+#' Extract resources from a set of evidence list columns
+#'
+#' @importFrom magrittr %>%
+#' @noRd
+resources_from <- function(data, ..., collapse = ';') {
+
+    extract_resources <- function(ev) {
+        paste0(ev$resource, `if`(is.null(ev$via), '', '_'), ev$via)
+    }
+
+    data %>%
+    chr_from(..., fn = extract_resources, collapse = collapse)
+
+}
+
+
+#' Extract references from a set of evidence list columns
+#'
+#' @importFrom magrittr %>%
+#' @noRd
+references_from <- function(data, ..., prefix = TRUE, collapse = ';') {
+
+    extract_references <- function(ev) {
+        `if`(
+            prefix,
+            paste(ev$resource, ev$references, sep = ':'),
+            ev$references, collapse = collapse
+        ) %>%
+        unique %>%
+        sort %>%
+        paste(collapse = collapse)
+    }
+
+    data %>%
+    chr_from(..., fn = extract_references, collapse = collapse)
+
+}
+
+
+#' Character vector from a set of list columns
+#'
+#' @importFrom magrittr %>%
+#' @importFrom purrr pmap_chr map_chr
+#' @importFrom dplyr select
+#' @noRd
+chr_from <- function(data, ..., fn, collapse = ';') {
+
+    map_fn <- function(...) {
+        c(...) %>%
+        map_chr(fn) %>%
+        paste(collapse = collapse) %>%
+        if_null_len0('')
+    }
+
+    data %>%
+    select(...) %>%
+    pmap_chr(map_fn)
+
+}
