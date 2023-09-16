482bba24b56af7e8ba5c4524419d9f9610e1a924
deeenes <turei.denes@gmail.com>
2023-09-17 00:23:25 +0200

    `static_table`: retrieve one static table from OmniPath

--- R/static.R
+++ R/static.R
@@ -39,6 +39,7 @@
 #' @importFrom stringr str_extract
 #' @importFrom lubridate dmy hm
 #' @export
+#' @seealso \code{\link{static_table}}
 static_tables <- function() {
 
     'omnipath_static' %>%
@@ -71,5 +72,134 @@ static_tables <- function() {
         size = as.integer(size),
         url = paste0(url_parser('omnipath_static'), fname)
     )
+
+}
+
+
+#' Retrieve a static table from OmniPath
+#'
+#' A few resources and datasets are available also as plain TSV files and
+#' can be accessed without TLS. The purpose of these tables is to make the
+#' most often used OmniPath data available on computers with configuration
+#' issues. These tables are not the recommended way to access OmniPath
+#' data, and a warning is issued each time they are accessed.
+#'
+#' @param query Character: a query type such as "annotations" or
+#'     "interactions".
+#' @param resource Character: name of the resource or dataset, such as
+#'     "CollecTRI" or "PROGENy".
+#' @param organism Integer: NCBI Taxonomy of the organism: 9606 for human,
+#'     10090 for mouse and 10116 for rat.
+#' @param strict_evidences Logical: restrict the evidences to the queried
+#'     datasets and resources. If set to FALSE, the directions and effect signs
+#'     and references might be based on other datasets and resources.
+#' @param wide Convert the annotation table to wide format, which
+#'     corresponds more or less to the original resource. If the data comes
+#'     from more than one resource a list of wide tables will be returned.
+#'     See examples at \code{\link{pivot_annotations}}.
+#' @param dorothea_levels Vector detailing the confidence levels of the
+#'     interactions to be downloaded. In dorothea, every TF-target interaction
+#'     has a confidence score ranging from A to E, being A the most reliable
+#'     interactions.
+#'     By default here we take A, B and C level interactions
+#'     (\code{c("A", "B", "C")}).
+#'     It is to note that E interactions are not available in OmnipathR.
+#'
+#' @return A data frame (tibble) with the requested resource.
+#'
+#' @examples
+#' static_table("annotations", "PROGENy")
+#'
+#' @importFrom logger log_warn log_error
+#' @importFrom magrittr %>% %<>% extract
+#' @importFrom stringr str_to_lower str_detect
+#' @importFrom dplyr pull filter
+#' @importFrom readr read_tsv cols
+#' @export
+#' @seealso \code{\link{static_tables}}
+static_table <- function(
+        query,
+        resource,
+        organism = 9606L,
+        strict_evidences = TRUE,
+        wide = TRUE,
+        dorothea_levels = c('A', 'B', 'C')
+    ) {
+
+    log_warn(
+         sprintf('Accessing `%s` as a static table: this is not ', resource),
+         'the recommended way to access OmniPath data; it is only a backup ',
+         'plan for situations when our server or your computer is ',
+         'experiencing issues.'
+    )
+
+    organism %<>% as.character %>% as.integer
+
+    query_l <- str_to_lower(query)
+    resource_l <- str_to_lower(resource)
+    datasets <- `if`(
+        resource_l %in% c('dorothea', 'collectri'),
+        resource_l,
+        NULL
+    )
+    resources <- `if`(is.null(datasets), resource_l, NULL)
+
+    static_tables() %>%
+    filter(
+        query == query_l &
+        str_to_lower(resource) == resource_l &
+        organism == organism
+    ) %>%
+    {`if`(
+        nrow(.) < 1L,
+        {
+            msg <-
+                paste0(
+                    'Static table not available for query `%s`, resource ',
+                    '`%s` and organism `%i`. For a list of available tables ',
+                    'see `static_tables()`.'
+                ) %>%
+                sprintf(query, resource, organism)
+            log_error(msg)
+            stop(msg)
+        },
+        .
+    )} %>%
+    pull(url) %>%
+    extract(1L) %>%
+    omnipath_download(
+        read_tsv,
+        col_types = cols(),
+        progress = FALSE,
+        show_col_types = FALSE
+    ) %>%
+    omnipath_post_download(
+         url = attr(., 'url'),
+         strict_evidences = strict_evidences,
+         param =
+             list(
+                 json_param = list(),
+                 query_type = query_l,
+                 datasets = datasets,
+                 resources = resources
+             ) %>%
+             add_qt_message
+    ) %>%
+    {`if`(
+        query_l == 'annotations' && wide,
+        pivot_annotations(.),
+        .
+    )} %>%
+    {`if`(
+        resource_l == 'dorothea',
+        filter(
+            .,
+            str_detect(
+                dorothea_level,
+                dorothea_levels %>% paste(collapse = '') %>% sprintf('[%s]', .)
+            )
+        ),
+        .
+    )}
 
 }
