b5e96934adc7555dbf9a88dfc97318b017b10f27
deeenes <turei.denes@gmail.com>
2024-08-06 13:23:52 +0200

    RaMP: URL & `ramp_sqlite`

---
+++ R/ramp.R
@@ -0,0 +1,34 @@
+#!/usr/bin/env Rscript
+
+#
+#  This file is part of the `OmnipathR` R package
+#
+#  Copyright
+#  2018-2024
+#  Saez Lab, Uniklinik RWTH Aachen, Heidelberg University
+#
+#  File author(s): Alberto Valdeolivas
+#                  Dénes Türei (turei.denes@gmail.com)
+#                  Attila Gábor
+#
+#  Distributed under the MIT (Expat) License.
+#  See accompanying file `LICENSE` or find a copy at
+#      https://directory.fsf.org/wiki/License:Expat
+#
+#  Website: https://r.omnipathdb.org/
+#  Git repo: https://github.com/saezlab/OmnipathR
+#
+
+
+#' Download and open RaMP database SQLite
+#'
+#' @noRd
+ramp_sqlite <- function(version = '2.5.4') {
+
+    path <-
+        'ramp' %>%
+        download_to_cache(url_param = list(version))
+
+    return(path)
+
+}
