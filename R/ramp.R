8f93c193e4a9206505eab8dd55a14430b447d66e
deeenes <turei.denes@gmail.com>
2024-08-06 14:08:50 +0200

    `ramp_sqlite` gunzips and opens SQLite

--- R/ramp.R
+++ R/ramp.R
@@ -22,13 +22,24 @@
 
 #' Download and open RaMP database SQLite
 #'
+#' @importFrom R.utils gunzip
+#' @importFrom RSQLite SQLite dbConnect
 #' @noRd
 ramp_sqlite <- function(version = '2.5.4') {
 
-    path <-
-        'ramp' %>%
-        download_to_cache(url_param = list(version))
+    fake_url <- version %>% sprintf('RAMP_%s.sqlite', .)
+    cache_record <- omnipath_cache_latest_or_new(url = fake_url)
 
-    return(path)
+    if (!cache_record$status == CACHE_STATUS$READY) {
+
+        'ramp' %>%
+        download_to_cache(url_param = list(version)) %>%
+        gunzip(destname = cache_record$path, remove = FALSE)
+
+        omnipath_cache_download_ready(cache_record)
+
+    }
+
+    dbConnect(SQLite(), cache_record$path)
 
 }
