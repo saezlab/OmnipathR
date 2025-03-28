e9e06ee9c7b8d252892603b46fda0bad2680eed5
deeenes <turei.denes@gmail.com>
2025-03-28 14:35:45 +0100

    `ramp`: use `sqlite_downloader`

--- R/ramp.R
+++ R/ramp.R
@@ -67,8 +67,6 @@ RAMP_GENE_ID_TYPES <- c(
 #' sqlite_con <- ramp_sqlite()
 #'
 #' @importFrom magrittr %>%
-#' @importFrom R.utils gunzip
-#' @importFrom RSQLite SQLite dbConnect
 #' @export
 #' @seealso \itemize{
 #'     \item{\code{\link{ramp_tables}}}
@@ -77,20 +75,7 @@ ramp_sqlite <- function(version = '2.5.4') {
 
     .slow_doctest()
 
-    fake_url <- version %>% sprintf('RAMP_%s.sqlite', .)
-    cache_record <- omnipath_cache_latest_or_new(url = fake_url)
-
-    if (!cache_record$status == CACHE_STATUS$READY) {
-
-        'ramp' %>%
-        download_to_cache(url_param = list(version)) %>%
-        gunzip(destname = cache_record$path, remove = FALSE)
-
-        omnipath_cache_download_ready(cache_record)
-
-    }
-
-    dbConnect(SQLite(), cache_record$path)
+    sqlite_downloader('ramp', version = version, compr = 'gz')
 
 }
 
