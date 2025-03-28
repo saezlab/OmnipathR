b71fb21d968e3273c26194eb7aaf20db06bd640a
deeenes <turei.denes@gmail.com>
2025-03-28 16:15:50 +0100

    added missing `metalinksdb.R`

---
+++ R/metalinksdb.R
@@ -0,0 +1,41 @@
+#!/usr/bin/env Rscript
+
+#
+#  This file is part of the `OmnipathR` R package
+#
+#  Copyright
+#  2018-2024
+#  Saez Lab, Uniklinik RWTH Aachen, Heidelberg University
+#
+#  File author(s): Alberto Valdeolivas
+#                  Dénes Türei (turei.denes@gmail.com)
+#                  Attila Gábor
+#
+#  Distributed under the MIT (Expat) License.
+#  See accompanying file `LICENSE` or find a copy at
+#      https://directory.fsf.org/wiki/License:Expat
+#
+#  Website: https://r.omnipathdb.org/
+#  Git repo: https://github.com/saezlab/OmnipathR
+#
+
+
+#' Open MetalinksDB as an SQLite3 connection
+#'
+#' MetalinksDB is a database of metabolite-protein and small molecule
+#' ligand-receptor interactions.
+#'
+#' @return An SQLite3 connection.
+#'
+#' @examples
+#' con <- metalinksdb_sqlite()
+#' con
+#'
+#' @importFrom magrittr %>%
+#' @export
+metalinksdb_sqlite <- function() {
+
+    'metalinksdb' %>%
+    sqlite_downloader()
+
+}
