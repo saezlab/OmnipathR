88af8128d57ed1f5ea83f785227d0beee8f978dd
Priscilla Bai <priscillabai@dhcp053-618.laptop-zim.uni-heidelberg.de>
2025-08-29 13:51:28 +0200

    create recon3d functions to access table of metabolism model

--- R/recon3d.R
+++ R/recon3d.R
@@ -16,9 +16,25 @@ recon3d_raw_matlab <- function() {
 
 #' @export
 recon3d_raw <- function() {
+    download_to_cache('recon3d_bigg_json') %>% 
+    safe_json()
+}
 
-    path <- download_to_cache('recon3d_bigg_json')
 
-    safe_json(path)
+#' @noRd
+recon3d_table <- function(name){
+   recon3d_raw() %>% extract2(name) %>% tibble()
+}
 
+recon3d_metabolites <- function(){
+  recon3d_table("metabolites")
+}
+recon3d_reactions <- function(){
+  recon3d_table("reactions")
+}
+recon3d_genes <- function(){
+  recon3d_table("genes")
+}
+recon3d_compartments <- function(){
+  recon3d_table("compartments")
 }
