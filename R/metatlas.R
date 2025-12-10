c667f5441c5156dece4b54767d75791803f6803d
deeenes <turei.denes@gmail.com>
2025-12-10 18:47:21 +0100

    `metabolic_atlas_model`: download an SBML model from Met. Atlas; first draft

--- R/metatlas.R
+++ R/metatlas.R
@@ -51,3 +51,83 @@ metabolic_atlas_models <- function() {
     select(-description2)
 
 }
+
+
+#' Download and load a Metabolic Atlas model
+#'
+#' Downloads a genome-scale metabolic model (GEM) from Metabolic Atlas and
+#' loads it using the SBMLR package (optional dependency).
+#'
+#' @param ... Arguments to filter models. Either:
+#'     - A data frame of filtered models from metabolic_atlas_models()
+#'     - Integer vector: model ID(s) from the `id` column
+#'     - Expressions passed to dplyr::filter() (e.g., organism == 'Homo sapiens')
+#'
+#' @return A model object loaded by SBMLR, or the path to the downloaded
+#'     SBML file if SBMLR is not available.
+#'
+#' @examples
+#' \dontrun{
+#' # Load model by ID
+#' model <- metabolic_atlas_model(1)
+#'
+#' # Load first human model
+#' model <- metabolic_atlas_model(organism == "Homo sapiens")
+#' }
+#'
+#' @importFrom magrittr %>%
+#' @importFrom rlang eval_tidy enexprs
+#' @importFrom dplyr filter slice
+#' @importFrom logger log_info log_error log_warn
+#' @export
+metabolic_atlas_model <- function(...) {
+
+    sbmlr_available <- requireNamespace("SBMLR", quietly = TRUE)
+
+    if (!sbmlr_available) {
+        msg <- "SBMLR package not available, install with: install.packages('SBMLR')"
+        log_warn(msg)
+        warning(msg, call. = FALSE)
+    }
+
+    args <- list(...)
+
+    # Handle different argument types
+    models <- if (length(args) == 1 && is.data.frame(args[[1]])) {
+        args[[1]]
+    } else if (length(args) == 1 && is.numeric(args[[1]])) {
+        metabolic_atlas_models() %>% filter(id %in% args[[1]])
+    } else {
+        filter_exprs <- enexprs(...)
+        models <- metabolic_atlas_models()
+        for (expr in filter_exprs) {
+            models <- models %>% filter(eval_tidy(expr, data = models))
+        }
+        models
+    }
+
+    # NSE vs. R CMD check workaround
+    id <- name <- files <- NULL
+
+    models <- models %>% slice(1)
+
+    sbml_path <- models$files[[1]] %>%
+        filter(format == "SBML") %>%
+        slice(1) %>%
+        pull(path)
+
+    log_info("Downloading model: %s (ID: %s)", models$name[1], models$id[1])
+
+    if (sbmlr_available) {
+        archive_extractor(
+            url_key = "metatlas_model",
+            url_param = list(sbml_path),
+            reader = SBMLR::readSBML
+        )
+    } else {
+        download_to_cache(
+            url_key = "metatlas_model",
+            url_param = list(sbml_path)
+        )
+    }
+}
